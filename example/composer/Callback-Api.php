1e6c8545eb3a29a6141a0be349bd56baf7230351
bambangm88 <bambangm88@gmail.com>
2022-07-08 14:58:24 +0700

    Update Callback-Api.php

--- example/composer/Callback-Api.php
+++ example/composer/Callback-Api.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$duitkuConfig = new \Duitku\Config("732B39FC61796845775D2C4FB05332AF", "D0001"); // 'YOUR_MERCHANT_KEY' and 'YOUR_MERCHANT_CODE'
+$duitkuConfig = new \Duitku\Config("732B39FC61XXXXXXXXXXXX", "D0XXX"); // 'YOUR_MERCHANT_KEY' and 'YOUR_MERCHANT_CODE'
 $duitkuConfig->setSandboxMode(true);
 // $duitkuConfig->setDuitkuLogs(false);
 
